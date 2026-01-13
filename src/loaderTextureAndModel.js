49e28bc6896a860a61e03a26973fceaa2ed3536b
Alexey Staburov <gilbertfrost@yandex.ru>
2026-01-13 13:10:33 +0300

    fix: resolve loading manager without assets

--- src/loaderTextureAndModel.js
+++ src/loaderTextureAndModel.js
@@ -3,15 +3,21 @@ import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const manager = new THREE.LoadingManager();
-export const loadingDone = new Promise((resolve) => {
+let started = false;
+const loadingDone = new Promise((resolve) => {
+  manager.onStart = () => {
+    started = true;
+  };
   manager.onLoad = () => resolve();
+  setTimeout(() => {
+    if (!started) resolve();
+  }, 0);
 });
 
-
+const loaderGLB = new GLTFLoader(manager);
+const loaderTexture = new THREE.TextureLoader(manager);
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/draco/');
 loaderGLB.setDRACOLoader(dracoLoader);
 
-
-const loaderGLB = new GLTFLoader(manager);
-const loaderTexture = new THREE.TextureLoader(manager);
+export { loaderGLB, loaderTexture, loadingDone };

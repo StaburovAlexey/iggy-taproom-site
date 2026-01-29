5410cf5bac0075ee1765302d69ca20c2db9598ec
Alexey Staburov <gilbertfrost@yandex.ru>
2026-01-30 00:10:00 +0300

    Fix build entry point

--- src/main.js
+++ src/main.js
@@ -28,25 +28,6 @@ const barPromise = loadBar();
 const peoplePromise = createPeopleModels();
 const menuHoverPromise = createMenuHoverModels(null, menuTarget);
 
-const models = await barPromise;
-applyLowPoly(models.bar, 0.01, 1500);
-applyPS1Style(models.bar);
-const { firstFrame, goToPoint, scene, addUpdate, renderer, camera } = createScene(app, models);
-const [peopleModels, menuHoverModels] = await Promise.all([
-  peoplePromise,
-  menuHoverPromise,
-]);
-if (peopleModels?.group) {
-  scene.add(peopleModels.group);
-}
-if (menuHoverModels?.group) {
-  scene.add(menuHoverModels.group);
-}
-applyPS1Style(peopleModels.group, '500.0');
-addUpdate(peopleModels.update);
-addUpdate(menuHoverModels.update);
-await warmupScene(renderer, scene, camera, [menuHoverModels?.group]);
-await Promise.all([loadingDone, firstFrame]);
 const menuWindow = document.createElement('menu-window');
 const menuListWindow = document.createElement('menu-list-window');
 const activityWindow = document.createElement('activity-window');
@@ -64,72 +45,101 @@ let defaultTrack = musicTracks.find(
 if (!defaultTrack && musicTracks.length > 0) {
   defaultTrack = musicTracks[0];
 }
+
 const tryPlayMusic = () => {
   if (!musicAudio.src) {
     return Promise.resolve();
   }
   musicAudio.muted = false;
-  return musicAudio.play().then(() => {
-    soundHint.close();
-  }).catch((error) => {
-    soundHint.open();
-    throw error;
-  });
+  return musicAudio
+    .play()
+    .then(() => {
+      soundHint.close();
+    })
+    .catch((error) => {
+      soundHint.open();
+      throw error;
+    });
 };
 
-if (defaultTrack) {
-  const src = `${baseUrl}music/${defaultTrack.file}`;
-  musicAudio.src = src;
-  musicListWindow.setActiveTrack(defaultTrack);
-  tryPlayMusic().catch(() => {});
-}
-menuWindow.addEventListener('navigate', (event) => {
-  currentPoint = event.detail;
-  goToPoint(event.detail);
-  if (currentPoint === 'activity') {
-    activityWindow.open();
-  } else {
-    activityWindow.close();
+async function init() {
+  const models = await barPromise;
+  applyLowPoly(models.bar, 0.01, 1500);
+  applyPS1Style(models.bar);
+  const { firstFrame, goToPoint, scene, addUpdate, renderer, camera } = createScene(app, models);
+  const [peopleModels, menuHoverModels] = await Promise.all([
+    peoplePromise,
+    menuHoverPromise,
+  ]);
+  if (peopleModels?.group) {
+    scene.add(peopleModels.group);
   }
-  if (currentPoint === 'music') {
-    musicListWindow.open();
-  } else {
-    musicListWindow.close();
+  if (menuHoverModels?.group) {
+    scene.add(menuHoverModels.group);
   }
-  if (currentPoint !== 'menu') {
-    menuHoverModels.hide();
-  }
-});
-menuWindow.addEventListener('menu-list-open', (event) =>
-  menuListWindow.open(event.detail),
-);
-menuWindow.addEventListener('menu-list-close', () => menuListWindow.close());
-musicListWindow.addEventListener('music-select', (event) => {
-  const { src } = event.detail;
-  if (!src) {
-    return;
-  }
-  if (musicAudio.src !== src) {
-    musicAudio.src = src;
-  }
-  tryPlayMusic().catch(() => {});
-  musicListWindow.setActiveTrack(event.detail);
-});
-menuWindow.addEventListener('menu-hover', (event) => {
-  if (currentPoint !== 'menu') {
-    return;
-  }
-  menuHoverModels.show(event.detail);
-});
-menuWindow.addEventListener('menu-hover-out', () => menuHoverModels.hide());
-document.body.appendChild(menuWindow);
-document.body.appendChild(menuListWindow);
-document.body.appendChild(activityWindow);
-document.body.appendChild(musicListWindow);
-document.body.appendChild(soundHint);
-stopProgress();
-preloader.hide();
+  applyPS1Style(peopleModels.group, '500.0');
+  addUpdate(peopleModels.update);
+  addUpdate(menuHoverModels.update);
+  await warmupScene(renderer, scene, camera, [menuHoverModels?.group]);
+  await Promise.all([loadingDone, firstFrame]);
 
-soundHint.addEventListener('sound-request', () => {
-  tryPlayMusic().catch(() => {});
-});
+  menuWindow.addEventListener('navigate', (event) => {
+    currentPoint = event.detail;
+    goToPoint(event.detail);
+    if (currentPoint === 'activity') {
+      activityWindow.open();
+    } else {
+      activityWindow.close();
+    }
+    if (currentPoint === 'music') {
+      musicListWindow.open();
+    } else {
+      musicListWindow.close();
+    }
+    if (currentPoint !== 'menu') {
+      menuHoverModels.hide();
+    }
+  });
+  menuWindow.addEventListener('menu-list-open', (event) =>
+    menuListWindow.open(event.detail),
+  );
+  menuWindow.addEventListener('menu-list-close', () => menuListWindow.close());
+  musicListWindow.addEventListener('music-select', (event) => {
+    const { src } = event.detail;
+    if (!src) {
+      return;
+    }
+    if (musicAudio.src !== src) {
+      musicAudio.src = src;
+    }
+    tryPlayMusic().catch(() => {});
+    musicListWindow.setActiveTrack(event.detail);
+  });
+  menuWindow.addEventListener('menu-hover', (event) => {
+    if (currentPoint !== 'menu') {
+      return;
+    }
+    menuHoverModels.show(event.detail);
+  });
+  menuWindow.addEventListener('menu-hover-out', () => menuHoverModels.hide());
+
+  if (defaultTrack) {
+    const src = `${baseUrl}music/${defaultTrack.file}`;
+    musicAudio.src = src;
+    musicListWindow.setActiveTrack(defaultTrack);
+    tryPlayMusic().catch(() => {});
+  }
+
+  document.body.appendChild(menuWindow);
+  document.body.appendChild(menuListWindow);
+  document.body.appendChild(activityWindow);
+  document.body.appendChild(musicListWindow);
+  document.body.appendChild(soundHint);
+  soundHint.addEventListener('sound-request', () => {
+    tryPlayMusic().catch(() => {});
+  });
+  stopProgress();
+  preloader.hide();
+}
+
+init().catch(() => {});
